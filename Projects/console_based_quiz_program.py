2f404a5454c521efd84780689e5f76c4375541c8
sumathi287 <sumathi.10287@gmail.com>
2026-05-27 17:33:05 +0530

    change the variable name,modify the print statement and update the quiz questions

--- Projects/console_based_quiz_program.py
+++ Projects/console_based_quiz_program.py
@@ -1,42 +1,47 @@
-target = input(
-    'Welcome to quiz program!\nEnter "yes" if you want to play quiz \nEnter "no" or any charecter to quit the quiz"\n'
-)
+target = input("Welcome to quiz program!\nDo you want to play?\n")
 count = 0
 temp_var = 0
-count_1 = 0
+score = 0
 if target == "yes":
+    print("okay lets play :)")
     user_input = input("1.What does RAM stand for?\n")
     if user_input.upper() == "random access memory".upper():
-        print("the answer is correct")
-        count_1 = +1
+        print("Correct!")
+        score += 1
         count = count + 25
     else:
-        print("The answer is incorrect\n")
+        print("Incorrect!")
     user_input = input('2.Which part of the computer is known as the "brain"?\n')
     if user_input.upper() == "CPU".upper():
-        print("The answer is correct")
-        count_1 = +count_1
+        print("Correct!")
+        score += 1
         count = count + 25
     else:
-        print("The answer is incorrect\n")
+        print("Incorrect!")
     user_input = input('3.What does "WWW" stand for when browsing the internet?\n')
     if user_input.upper() == "World Wide Web".upper():
-        print("The answer is correct")
-        count_1 = +count_1
+        print("Correct!")
+        score += 1
         count = count + 25
     else:
-        print("The answer is incorrect\n")
+        print("Incorrect!")
     user_input = input(
         "4.Which device is used to physically type text into a computer?\n"
     )
     if user_input.upper() == "Keyboard".upper():
-        print("The answer is correct")
-        count_1 = +count_1
+        print("Correct!")
+        score += 1
         count = count + 25
     else:
-        print("The answer is incorrect\n")
+        print("Incorrect!")
+    user_input = input("5.What is something you simply cannot live without??\n")
+    if user_input.upper() == "sumi".upper() or user_input.upper() == "sumathi".upper():
+        print(f"Correct Honey...{"\u2665"}{"\u2665"}{"\u2665"}!")
+        score += 1
+        count = count + 25
+    else:
+        print("Incorrect,I realy hate you...!")
 else:
-    print("Exit from the quiz program\n")
-print(f"the total correct answers is {count_1}\n")
-print(f"the total percentage of correct answer is {count}%\n")
-print("End the quiz!!")
+    quit()
+print(f"you got {score } questions is correct\n")
+print(f"you got {(score /5) * 100} %")
