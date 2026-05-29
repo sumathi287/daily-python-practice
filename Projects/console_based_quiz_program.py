5ae8f32ccfc5e8cdb92d62617e467933312cbecb
sumathi287 <sumathi.10287@gmail.com>
2026-05-29 09:25:21 +0530

    modified:   Projects/console_based_quiz_program.py - modify the exit statement in else part,add the .strip function for input string for removing space, add the comments

--- Projects/console_based_quiz_program.py
+++ Projects/console_based_quiz_program.py
@@ -1,54 +1,51 @@
-# it would be nice to have description of project
-
+import sys
+#global variables
 target = input("Welcome to quiz program!\nDo you want to play?\n")
 count = 0
 temp_var = 0
 score = 0
-# execute the block of if statement based on user input
+# execute the block of if else statement based on user input
 if "yes" in target.lower():
     print("okay lets play :)")
 
     user_input = input("1.What does RAM stand for?\n")
+    user_input = user_input.strip() #remove the space at the starting and ending of the string
     if user_input.upper() == "random access memory".upper():
         print("Correct!")
         score += 1
-        count = count + 25 # what is the use of this 'count' variable
+        count = count + 25  # what is the use of this 'count' variable
     else:
         print("Incorrect!")
-        
+
     user_input = input('2.Which part of the computer is known as the "brain"?\n')
-    if user_input.upper() == "CPU".upper(): #Nice!
+    user_input = user_input.strip() #remove the space at the starting and ending of the string
+    if user_input.upper() == "CPU".upper():  # Nice!
         print("Correct!")
         score += 1
         count = count + 25
     else:
         print("Incorrect!")
-        
+
     user_input = input('3.What does "WWW" stand for when browsing the internet?\n')
-    if user_input.upper() == "World Wide Web".upper(): # Nice
+    user_input = user_input.strip() #remove the space at the starting and ending of the string
+    if user_input.upper() == "World Wide Web".upper():  # Nice
         print("Correct!")
         score += 1
         count = count + 25
     else:
         print("Incorrect!")
-        
+
     user_input = input(
         "4.Which device is used to physically type text into a computer?\n"
     )
+    user_input = user_input.strip() #remove the space at the starting and ending of the string
     if user_input.upper() == "Keyboard".upper():
         print("Correct!")
         score += 1
         count = count + 25
     else:
         print("Incorrect!")
-    user_input = input("5.What is something you simply cannot live without??\n")
-    if user_input.upper() == "sumi".upper() or user_input.upper() == "sumathi".upper():
-        print(f"Correct Honey...{"\u2665"}{"\u2665"}{"\u2665"}!")
-        score += 1
-        count = count + 25
-    else:
-        print("Incorrect,I realy hate you...!")
 else:
-    quit() # could provide exit code!
+    sys.exit("exit the code successfully!!")  # could provide exit code!
 print(f"you got {score } questions is correct\n")
-print(f"you got {(score /4) * 100} %")
+print(f"you got {(score /4) * 100}% score")
