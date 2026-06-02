e01bd24b0e4eb61eb83153a46d72248ff38964fc
sumathi287 <sumathi.10287@gmail.com>
2026-06-02 08:53:31 +0530

    Projects\number_guessing_game_2.py - Add the global variable,create new function,modified the game logic program

--- Projects/number_guessing_game_2.py
+++ Projects/number_guessing_game_2.py
@@ -4,34 +4,57 @@ from sys import exit
 i = ["ROCK", "PAPER", "SCISSORS"]
 random_choice = choice(i)
 
-temp = input('Do you want to play?:\nPress "Enter" key if you want\n')
+temp = input(
+    'Do you want to play?:\nEnter "y" if you want play\nEnter "n" if you dont want play\n'
+)
+temp = temp.upper()
+temp_var = "Y"
+
+
+def computer_selection():
+    print(f"computer picked {random_choice.lower()}")
+
+
 def play():
     user_input = input("Type Rock/Paper/Scissors:")
-    print(f"computer picked {random_choice}")
     user_input = user_input.upper()
     if (random_choice == "ROCK") and (user_input == "ROCK"):
+        computer_selection()
         print("It's a tie")
     elif (random_choice == "PAPER") and (user_input == "PAPER"):
+        computer_selection()
         print("It's a tie")
     elif (random_choice == "SCISSORS") and (user_input == "SCISSORS"):
+        computer_selection()
         print("It's a tie")
     elif (random_choice == "ROCK") and (user_input == "PAPER"):
+        computer_selection()
         print("You won!")
     elif (random_choice == "ROCK") and (user_input == "SCISSORS"):
+        computer_selection()
         print("You lost!")
     elif (random_choice == "PAPER") and (user_input == "ROCK"):
+        computer_selection()
         print("You lost!")
     elif (random_choice == "PAPER") and (user_input == "SCISSORS"):
+        computer_selection()
         print("You won!")
     elif (random_choice == "SCISSORS") and (user_input == "ROCK"):
+        computer_selection()
         print("You lost!")
     elif (random_choice == "SCISSORS") and (user_input == "PAPER"):
-        print("You won!")
+        computer_selection()
+        print("You lost!")
     else:
-        print("enter wrong string!!\n")
+        print("enter a wrong string!!\n")
 
 
-if temp == "":
-    play()
-else:
-    exit("Quit the program!!")
+while temp_var == "Y":
+    if temp == "Y":
+        play()
+        temp_var = input(
+            f'Enter "y" if you continue the game\nEnter "n" or any charecter if quit the game\n'
+        )
+        temp_var = temp_var.upper()
+    else:
+        exit("Quit the program!!")
---
+++ README.md
@@ -0,0 +1,29 @@
+# Python Practice Programs
+
+This repository contains beginner to advanced Python Projects.
+
+## Topics Covered
+
+### 1. Basics
+- Hello World
+- Variables
+- Input/Output
+- Operators
+
+### 2. Loops
+
+### 3. Strings
+
+### 4. Lists
+
+
+### 5. Functions
+
+
+### 6. Mini Projects
+- Using the print function to output information to the user
+## Goal
+To improve Python programming and logic-building skills through practice small projects.
+
+## Author
+Sumathi Periarakki
